7689b276b37ed266cafaf426fa7b476b2ca13f36
YaminiSushmaBoddu <yaminisushma2828@gmail.com>
2023-01-01 13:19:29 +0530

    Salavuddin

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-//import './App.css';
+import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
