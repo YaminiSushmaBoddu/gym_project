89c0a10126dca3904f62f13f2b6d5a21b381e84c
Ravitejatorrikonda <ravitejatorrikonda@gmail.com>
2023-01-09 22:26:22 +0530

    login page

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import { useState} from 'react';
+
 
 const Login = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const handileemail=(e)=>{
+    setEmail(e.target.value)
+  }
+  const handilepassword=(e)=>{
+    setPassword(e.target.value)
+  } 
+  const handileclick=()=>{
+    console.log({email,password})
+  }
+  
   return (
-    <div>Login</div>
+    <div className='app'>
+<form>
+  <input type="email" value={email} onChange={handileemail}/><br/>
+  <input type="password" value={password} onChange={handilepassword}/>
+  <button onClick={handileclick}>login</button>
+  </form>
+    </div>
   )
 }
 
