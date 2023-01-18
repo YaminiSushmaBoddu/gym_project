97796d85e797dedaa984771588057a8f9004b857
Ravitejatorrikonda <ravitejatorrikonda@gmail.com>
2023-01-18 09:16:39 +0530

    new changes on login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios';
+import './Login.css'
+import { useNavigate } from 'react-router-dom';
 
 
 const Login = () => {
@@ -13,14 +15,19 @@ const Login = () => {
   const handilepassword = (e) => {
     setPassword(e.target.value)
   }
-  const handileclick = () => {
-    axios.post('http://localhost:3000/users', {
+
+  const navigate =useNavigate()
+  const handileclick = (e) => {
+    e.preventDefault()
+    axios.post('http://localhost:3007/users', {
       email:email,
       password:password
     }).then(res=>{
       console.log(res);
       alert('success');
-      <h1>login succesfully completed</h1>
+      navigate("/")
+      // console.log("login successfull");
+    
 
     }).catch(error=>{
       alert('server error')
@@ -31,11 +38,34 @@ const Login = () => {
 
   return (
     <div className='app'>
-      <form>
-        <input type="email" value={email} onChange={handileemail} /><br />
-        <input type="password" value={password} onChange={handilepassword} /><br />
-        <button onClick={handileclick}>login</button>
+      <div className='box'>
+      <form onSubmit={handileclick}>
+        <div className='formbody'>
+          <h3>sign in</h3>
+          <div className='inputbox'>
+          <span>Username : </span>
+          <input type="email" value={email} onChange={handileemail} placeholder="Enter user name"/><br />
+         
+          <i></i>
+          </div>
+          <div className='inputbox'>
+          <span>Password : </span>
+          <input type="password" value={password} onChange={handilepassword} placeholder="enter password" /><br />
+          
+          <i></i>
+          </div>
+
+    
+       
+       <div className='btnbutton'>
+       <button type='submit'><span>login</span></button>
+       </div>
+       
+    
+        </div>
       </form>
+      </div>
+     
     </div>
   )
 }
